7181e1281faa07f8617dbe32bf9a94b03439f580
Stanislaw Osinski <stanislaw.osinski@carrotsearch.com>
2021-09-20 17:22:27 +0200

    @carrotsearch/gatsby-theme-apidocs: set code highlighting theme.

--- gatsby-theme-apidocs/gatsby-browser.js
+++ gatsby-theme-apidocs/gatsby-browser.js
@@ -4,4 +4,4 @@ import "./styles/theme.css";
 import "./styles/apidocs.css";
 import "typeface-lato";
 import "typeface-raleway";
-import "highlight.js/styles/paraiso-dark.css"
+import "highlight.js/styles/base16/eighties.css"
