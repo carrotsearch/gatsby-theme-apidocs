876f5d147c85859d48f2c4e46ddfd7022357549a
Stanislaw Osinski <stanislaw.osinski@carrotsearch.com>
2021-09-29 18:36:19 +0200

    @carrotsearch/gatsby-theme-apidocs: preserve main navigation scroll position between article reloads.

--- gatsby-theme-apidocs/gatsby-browser.js
+++ gatsby-theme-apidocs/gatsby-browser.js
@@ -5,11 +5,29 @@ import "./styles/apidocs.css";
 import "./styles/hljs.css";
 import "typeface-lato";
 import "typeface-raleway";
-import "highlight.js/styles/base16/eighties.css"
+import "highlight.js/styles/base16/eighties.css";
 
 import { Lightbox } from "./components/carrotsearch/lightbox.js";
 import "./components/carrotsearch/lightbox.css";
 
 export const onInitialClientRender = () => {
   new Lightbox();
+};
+
+// Preserve main navigation scrolling position between article reloads.
+let lastMainNavScrollTop = undefined;
+export const onPreRouteUpdate = () => {
+  const mainNav = document.querySelector("nav.main > ul");
+  if (mainNav) {
+    lastMainNavScrollTop = mainNav.scrollTop;
+  }
+};
+
+export const onRouteUpdate = () => {
+  if (lastMainNavScrollTop !== undefined) {
+    const mainNav = document.querySelector("nav.main > ul");
+    if (mainNav) {
+      mainNav.scrollTo(0, lastMainNavScrollTop);
+    }
+  }
 };
